e62cf8fc07526420e34089279358ad62c08f4d78
tniromin <tniromin2@gmail.com>
2024-05-19 17:54:09 +0000

    UPDATE: file

---
+++ test.bash
@@ -0,0 +1,34 @@
+repo=$1
+dir=$2
+
+# Configure gitbash before using these commands
+# git config --global user.name "Tniromin"
+# git config --global user.email "tniromin2@gmail.com"
+
+
+# Use personal access token in place of pass wod
+
+## For errors
+# git reset --hard
+
+if [ "$#" -ne 2 ];then
+    echo "Usage: gitcmd.bash <URL-to-Repo> <Directory-Name>"
+    echo "Using custom DIR name & Custom Test Repo"
+    repo="https://github.com/tniromin/test.git "
+    dir="repo"
+fi
+
+git clone $repo $dir
+git remote add origin $repo
+
+git checkout -b dev
+
+echo "$(date)" >> $dir/file.md
+
+git add .
+git commit -m "UPDATE: file"
+git checkout main
+git merge dev
+
+git push -u origin main
+git remote remove origin
